d99867d9d7ad91e42c2c04eb55ac101af2f44da7
Aryavikram Pingali <aryavikrampingali@gmail.com>
2026-07-10 20:09:51 +0530

    Fixing Bugs

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,32 +1,11 @@
 "use client";
 
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [message, setMessage] = useState("Loading...");
-
-  useEffect(() => {
-    async function loadData() {
-      try {
-        const response = await fetch(
-          `${"http://localhost:8000"}/changedRoute`
-        );
-
-        const data = await response.json();
-        setMessage(data.message);
-      } catch (error) {
-        console.error(error);
-        setMessage("Failed to connect");
-      }
-    }
-
-    loadData();
-  }, []);
 
   return (
     <main>
-      <h1>Chat With Documents</h1>
-      <p>{message}</p>
+      <h1>Docuchat</h1>
     </main>
   );
 }
